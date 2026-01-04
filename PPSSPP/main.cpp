954efa03947b26bf4c37ab211d9205b65d9fdc29
MMaZaHaKa <dsfsdfsdfsewdawdfsdf@gmail.com>
2026-01-04 19:02:04 +0200

    upd psp plg

--- PPSSPP/main.cpp
+++ PPSSPP/main.cpp
@@ -309,6 +309,13 @@ void PrintCurrDir()
 	printf("Current directory: %s\n", buffer);
 }
 
+float GetRandomFloatInc(float low, float high) {
+	if (low == high) return low;
+	if (low > high) std::swap(low, high);
+	float t = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
+	return low + t * (high - low);
+}
+
 inline bool BTN(int c) { return (GetAsyncKeyState(c) & 0x8000); }
 
 // MEM_MAPPED -> SPACE - > 1ST BIN BYTE
@@ -352,8 +359,6 @@ struct CControllerState
 	__int16 LeftShock;
 	__int16 RightShock;
 };
-
-/* 518 */
 struct CPad
 {
 	__int16 field_0;
@@ -368,12 +373,405 @@ struct CPad
 	char field_B1[31];
 	float field_D0;
 };
+struct CVuVector
+{
+	float x;
+	float y;
+	float z;
+	float w;
+};
+struct __declspec(align(4)) RwV3d
+{
+	float x;
+	float y;
+	float z;
+};
+struct RwMatrix
+{
+	RwV3d right;
+	int field_C;
+	RwV3d up;
+	int field_1C;
+	RwV3d forvard_at;
+	int field_2C;
+	RwV3d pos;
+	int field_3C;
+};
+void inline SetCVector4VU(CVuVector* p1, CVuVector* p2) { if (p1 && p2) { memcpy(p1, p2, 3 * 4); } }
+void inline SetRWV3D(RwV3d* p1, CVuVector* p2) { memcpy(p1, p2, 3 * 4); }
+struct CMatrix
+{
+	CVuVector right;
+	CVuVector at_forward;
+	CVuVector up;
+	CVuVector pos;
+	RwMatrix* m_pRwMat;
+};
+struct CPlaceable
+{
+	CMatrix m_pMat;
+};
+struct __declspec(align(4)) RwLLLink
+{
+	RwLLLink* next;
+	RwLLLink* prev;
+};
+struct RwLinkList
+{
+	RwLLLink link;
+};
+struct RwObject
+{
+	char type;
+	char subType;
+	char flags;
+	char privateFlags;
+	RwObject* parent;
+};
+struct RwObjectHasFrame
+{
+	RwObject object;
+	RwLLLink lFrame;
+	int sync;
+};
+struct ClumpExt
+{
+	int visibilityCallBack;
+	int alpha;
+};
+struct CRGBA
+{
+	unsigned __int8 red;
+	unsigned __int8 green;
+	unsigned __int8 blue;
+	unsigned __int8 alpha;
+};
+struct RwTexDictionary;
+struct RwRaster
+{
+	int unk1;
+	int unk2;
+	char* data;
+	int flags;
+};
+struct RwTexture
+{
+	RwRaster* raster;
+	RwTexDictionary* dict;
+	RwLLLink lInDictionary;
+	char name[32];
+	char mask[32];
+	int refCount; // ?
+	int field_54; // ?
+};
+struct RpMaterial
+{
+	RwTexture* texture;
+	CRGBA color;
+	__int16 refCount;
+	__int16 pad;
+	int unk2;
+};
+struct RpMaterialList
+{
+	RpMaterial** materials;
+	int numMaterials;
+	int space;
+};
+struct sPspGeometry
+{
+	int size;
+	int flags;
+	int numStrips;
+	int unk1;
+	float bound[4];
+	float scale[3];
+	int numVerts;
+	float pos[3];
+	int unk2;
+	int offset;
+	float unk3;
+};
+struct sPspGeometryMesh
+{
+	int offset;
+	__int16 numTriangles;
+	__int16 matID;
+	float unk1;
+	float uvScale[2];
+	float unk2[4];
+	float unk3;
+	char bonemap[8];
+};
+struct RpGeometry
+{
+	RwObject object;
+	__int16 refCount;
+	__int16 pad1;
+	RpMaterialList matList;
+	int pSkinPlg;
+	int pad2;
+	sPspGeometry msPspGeometry;
+	sPspGeometryMesh msPspGeometryMesh;
+};
+struct CAnimBlendClumpData
+{
+	//CAnimBlendLink link;
+	//int numFrames;
+	//CVuVector* velocity2d;
+	//AnimBlendFrameData* frames;
+};
+struct RpClump
+{
+	RwObject object;
+	RwLinkList atomicList;
+	ClumpExt ClumpExt;
+	CAnimBlendClumpData* pClumpAnimDataPlugin;
+};
+#define RpAtomic_fromClump(ptr) ( (RpAtomic*) (((uint8_t*)ptr) - 0x1C) ) // RslElementGroupForAllElements + inElementGroupLink__inClump: link ptr - 0x1C = pAtomic*
+struct AtomicExt
+{
+	__int16 modelId;
+	__int16 flags;
+};
+struct HAnimNodeInfo
+{
+	char field_0;
+	char field_1;
+	char flags;
+	char field_3;
+	int field_4;
+};
+struct RpHAnimHierarchy
+{
+	int flags;
+	int numNodes;
+	int pCurrentAnim;
+	int currentTime;
+	int pNextFrame;
+	int pAnimCallBack;
+	int pAnimCallBackData;
+	int animCallBackTime;
+	int pAnimLoopCallBack;
+	int pAnimLoopCallBackData;
+	int pMatrixArray;
+	int pMatrixArrayUnaligned;
+	HAnimNodeInfo* pNodeInfo;
+	int field_34;
+};
+struct RpAtomic
+{
+	RwObjectHasFrame object;
+	RpGeometry* geometry;
+	RpClump* clump;
+	RwLLLink inElementGroupLink;
+	void* renderCallBack;
+	AtomicExt AtomicExt;
+	RpHAnimHierarchy* pAtomicHAnimHierarchyPlugin;
+	int field_30;
+};
+enum RwObjectType
+{
+	rpFRAME = 0,
+	rpATOMIC = 1,
+	rpCLUMP = 2,
+	rpLIGHT = 3,
+	rpCAMERA = 4,
+	rp5 = 5,
+	rpTEXDICTIONARY = 6,
+	rpWORLD = 7,
+	rpGEOMETRY = 8,
+};
+const char* GetRwObjectDescByType(int objtype)
+{
+	switch (objtype) {
+	case rpFRAME: return "FRAME";
+	case rpATOMIC: return "ATOMIC";
+	case rpCLUMP: return "CLUMP";
+	case rpLIGHT: return "LIGHT";
+	case rpCAMERA: return "CAMERA";
+	case rp5: return "reserved";
+	case rpTEXDICTIONARY: return "TEXDICTIONARY";
+	case rpWORLD: return "WORLD";
+	case rpGEOMETRY: return "GEOMETRY";
+	default: return "Unknown case";
+	}
+}
+union uRslObjects
+{
+	RwObject* m_rwObject;
+	RpClump* m_rpClump;
+	RpAtomic* m_rpAtomic;
+};
+struct CEntity
+{
+	CPlaceable CPlaceable;
+	char CE_flags_A;
+	char CE_flags_B;
+	char CE_flags_C;
+	char CE_flags_D;
+	char _CE_flags_E;
+	char CE_flags_F;
+	char CE_flags_G;
+	char CE_flags_H;
+	char CE_flags_I;
+	char CE_flags_J;
+	char CE_flags_K;
+	char CE_flags_L;
+	//RpAtomic* m_rwObject;
+	uRslObjects m_urwObject;
+	__int16 m_scanCode;
+	__int16 m_modelIndex;
+	__int16 m_modelIndex2;
+	char flags_field_5A;
+	char m_lastWepDam;
+	void* vftable;
+};
+
+/* 486 */
+struct CPlayerInfo
+{
+	CEntity* m_pPed;
+	CEntity* m_pRemoteVehicle;
+	char field_8[180];
+	int m_nMoney;
+	char pppaaaddd[28];
+	int m_pHooker;
+	char field_E0[24];
+	int field_F8;
+	char field_FC[4];
+	int m_nTimeTankShotGun;
+	char field_104[140];
+}; // size done
 
 
 
+#define CWorld_Players ((void*)IDATRANSLATE(0x08BDE4B0))
+#define CWorld_PlayerInFocus ((*(uint8_t*)IDATRANSLATE(0x08BB3BD8)))
 #define CPad_Pads ((CPad*)IDATRANSLATE(0x08BDE610))
 
 
+
+int GetEntityType(CEntity* pEntity) { int m_type = ((pEntity->_CE_flags_E >> 1) & 0x07); return m_type; }
+void SetEntityType(CEntity* pEntity, int type) { pEntity->_CE_flags_E &= ~(0x07 << 1); pEntity->_CE_flags_E |= (type & 0x07) << 1; }
+int GetEntityStatus(CEntity* pEntity) { int m_status = ((pEntity->_CE_flags_E >> 4) & 0x0F) | ((pEntity->CE_flags_F & 0x01) << 4); return m_status; }
+void SetEntityStatus(CEntity* pEntity, int st)
+{
+	pEntity->_CE_flags_E &= ~(0x0F << 4); pEntity->_CE_flags_E |= (st & 0x0F) << 4; pEntity->CE_flags_F &= ~0x01; pEntity->CE_flags_F |= (st >> 4) & 0x01;
+}
+
+void TeleportEntity(CEntity* pE, CVuVector pos, bool updrw = true)
+{
+	//if (pE) { pE->CPlaceable.m_pMat.pos.x = pos.x; pE->CPlaceable.m_pMat.pos.y = pos.y; pE->CPlaceable.m_pMat.pos.z = pos.z; }
+	if (pE) { SetCVector4VU(&pE->CPlaceable.m_pMat.pos, &pos); }
+	if (pE && updrw && pE->CPlaceable.m_pMat.m_pRwMat) { SetRWV3D(&pE->CPlaceable.m_pMat.m_pRwMat->pos, &pos); } // todo update rw stuff
+}
+//inline CBaseModelInfo* GetModelInfo(int index) { return EMUPOINTER<CBaseModelInfo*>(CModelInfo_ms_modelInfoPtrs[index]); }
+CEntity* FindPlayerPed() { return EMUPOINTER<CEntity*>(((CPlayerInfo*)CWorld_Players)[CWorld_PlayerInFocus].m_pPed); }
+//CEntity* FindPlayerVehicle() {
+//	CPed* pPed = EMUPOINTER<CPed*>(((CPlayerInfo*)CWorld_Players)[CWorld_PlayerInFocus].m_pPed);
+//	return EMUPOINTER<CVehicle*>(pPed ? pPed->m_pMyVehicle : null);
+//}
+//CVector FindPlayerPos() { return FindPlayerPed() ? (*(CVuVector*)&FindPlayerPed()->CPed.CPhysical.CEntity.CPlaceable.m_pMat.pos) : CVector{ 0, 0, 0 }; }
+//CVector FindPlayerMenuTarget() {
+//	CVuVector pos = { 0, 0, 0 };
+//	for (int i = 0; i < 75; i++) { // NUMRADARBLIPS
+//		if (TheRadar->ms_RadarTrace[i].m_eRadarSprite == eRadarSprite::RADAR_SPRITE_MP_OBJECTIVE) { pos = TheRadar->ms_RadarTrace[i].m_vecPos; break; }
+//	}
+//	return { pos.x, pos.y, (pos.z == 0.0f && pos.x != 0.0f && pos.y != 0.0f) ? 12.0f : pos.z };
+//}
+
+void UpdPad()
+{
+	bool w = BTN('W');
+	bool s = BTN('S');
+	bool a = BTN('A');
+	bool d = BTN('D');
+	//bool esc = BTN(VK_ESCAPE); // used by ppsspp
+	bool esc = BTN('R');
+
+	int nPad = 0;
+	CPad_Pads[nPad].NewState.DPadUp = w ? 0xFF : 0;
+	CPad_Pads[nPad].NewState.DPadDown = s ? 0xFF : 0;
+	CPad_Pads[nPad].NewState.DPadLeft = a ? 0xFF : 0;
+	CPad_Pads[nPad].NewState.DPadRight = d ? 0xFF : 0;
+	CPad_Pads[nPad].NewState.Start = esc ? 0xFF : 0;
+
+	CPad_Pads[nPad].OldState.DPadUp = w ? 0xFF : 0;
+	CPad_Pads[nPad].OldState.DPadDown = s ? 0xFF : 0;
+	CPad_Pads[nPad].OldState.DPadLeft = a ? 0xFF : 0;
+	CPad_Pads[nPad].OldState.DPadRight = d ? 0xFF : 0;
+	CPad_Pads[nPad].OldState.Start = esc ? 0xFF : 0;
+
+	int* pUpdJal = EMUPOINTER<int*>(0x0898B45C);
+	*pUpdJal = 0; // disable pad update, todo reset recompiler cache?
+}
+
+void UpdMP()
+{
+	// dis timeout
+}
+
+void UpdTest1()
+{
+	CEntity* pPlayer = FindPlayerPed();
+	printf("pPlayer: 0x%p\n", pPlayer);
+	if (!pPlayer)
+		return;
+	RpClump* pClump = EMUPOINTER<RpClump*>(pPlayer->m_urwObject.m_rpClump);
+	//printf("rwobj type id: %d %s\n", rwo->type, rwo->type == rpATOMIC ? "atomic" : "clump");
+	printf("pClump: 0x%p %s\n", pClump, GetRwObjectDescByType(pClump->object.type));
+
+
+	RwLLLink* head = &pClump->atomicList.link;
+	//debug("head : 0x%p\n", head); // field clump
+	RpAtomic a;
+	assert(((uint8_t*)&a) == ((uint8_t*)&a.inElementGroupLink - 0x1C)); // stru test
+
+	int i = 0;
+	//for (RwLinkList* link = EMUPOINTER<RwLinkList*>(clump->atomicList.link.next); link; link = EMUPOINTER<RwLinkList*>(link->link.next)) // wrong looping
+	for (RwLLLink* link = EMUPOINTER<RwLLLink*>(head->next); link != head; link = EMUPOINTER<RwLLLink*>(link->next)) // FORLIST
+	{
+		RpAtomic* atomic = RpAtomic_fromClump(link); // FORLIST
+		//printf("ATOMIC!!! [%d] : 0x%p   lnk 0x%p\n", i++, atomic, link);
+		printf("atomic: 0x%p %s\n", atomic, GetRwObjectDescByType(atomic->object.object.type));
+		RpGeometry* geo = EMUPOINTER<RpGeometry*>(atomic->geometry);
+		//float s = GetRandomFloatInc(0.5f, 2.5f);
+		float s = 1.2f;
+		//geo->msPspGeometry.scale[0] = s;
+		//geo->msPspGeometry.scale[1] = s;
+		//geo->msPspGeometry.scale[2] = s;
+		//geo->msPspGeometryMesh.matID -= -1;
+		geo->msPspGeometry.numStrips = 5;
+		//geo->msPspGeometry.pos[0] = 2.3f;
+		//geo->msPspGeometry.pos[1] = 2.3f;
+		//geo->msPspGeometry.pos[2] = 2.3f;
+		geo->msPspGeometryMesh.numTriangles = 0;
+		printf("geo->msPspGeometry: 0x%X\n", &geo->msPspGeometry);
+	}
+
+
+
+	//{
+	//	for (int32_t i = CPools_ms_pPedPool->m_nSize - 1; i >= 0; i--)
+	//	{
+	//		if (CPools_GetSlotIsFree(CPools_ms_pPedPool, i)) { continue; }
+	//		CPed* p = (CPed*)CPools_GetSlot(CPools_ms_pPedPool, i, 3360);
+	//		if (p) {
+	//			int32_t mindex = p->CPhysical.CEntity.m_modelIndex;
+	//			CBaseModelInfo* mi = GetModelInfo(mindex);
+	//			printf("ped[%d]: 0x%p, mi: 0x%p\n", p->CPhysical.CEntity.m_modelIndex, p, mi);
+	//			// test code
+	//			{
+	//				int8_t& b1D8 = *OFFSET(p, 0x1D8, int8_t*);
+	//				SWAP_BIT(b1D8, 3);
+	//				//bool IsDBY = b1D8 & BIT(3);
+	//				//b1D8 = IsDBY ? (b1D8 & (~BIT(3))) : (IsDBY | BIT(3));
+	//			}
+	//		}
+	//	}
+	//}
+}
 
 bool quit = false;
 void PluginInit()
@@ -385,6 +783,7 @@ void PluginInit()
 
 void PluginLoop()
 {
+	quit = BTN('Q');
 	//if (BTN('R'))
 	if (!PPSSPP_BASE) // todo OnLoadGame/State Event!!!!
 	{ // temp hack to find region
@@ -408,28 +807,9 @@ void PluginLoop()
 		return;
 	}
 
-	bool w = BTN('W');
-	bool s = BTN('S');
-	bool a = BTN('A');
-	bool d = BTN('D');
-	//bool esc = BTN(VK_ESCAPE); // used by ppsspp
-	bool esc = BTN('R');
-
-	int nPad = 0;
-	CPad_Pads[nPad].NewState.DPadUp = w ? 0xFF : 0;
-	CPad_Pads[nPad].NewState.DPadDown = s ? 0xFF : 0;
-	CPad_Pads[nPad].NewState.DPadLeft = a ? 0xFF : 0;
-	CPad_Pads[nPad].NewState.DPadRight = d ? 0xFF : 0;
-	CPad_Pads[nPad].NewState.Start = esc ? 0xFF : 0;
-
-	CPad_Pads[nPad].OldState.DPadUp = w ? 0xFF : 0;
-	CPad_Pads[nPad].OldState.DPadDown = s ? 0xFF : 0;
-	CPad_Pads[nPad].OldState.DPadLeft = a ? 0xFF : 0;
-	CPad_Pads[nPad].OldState.DPadRight = d ? 0xFF : 0;
-	CPad_Pads[nPad].OldState.Start = esc ? 0xFF : 0;
-
-	int* pUpdJal = EMUPOINTER<int*>(0x0898B45C);
-	*pUpdJal = 0; // disable pad update, todo reset recompiler cache?
+	UpdPad();
+	UpdMP();
+	UpdTest1();
 
 	//printf("%s\n", PSPPOINTER(0x08BB153C));
 	//printf("%s\n", PSPPOINTER(0x08BAD5C0));
@@ -438,6 +818,9 @@ void PluginLoop()
 
 DWORD CALLBACK ThreadEntry(LPVOID)
 {
+	if(BTN(VK_SHIFT))
+		return TRUE;
+
 	//WaitElf();
 	PluginInit();
 	while (!quit)
